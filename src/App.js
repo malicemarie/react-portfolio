e96fcf1a3a904af191ec03fdf862e78a7919063a
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-04-21 12:08:40 -0700

    removed contact page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
 
 import Portfolio from "./Components/Portfolio";
 
@@ -48,8 +47,6 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
-
-        <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
       </div>
     );
