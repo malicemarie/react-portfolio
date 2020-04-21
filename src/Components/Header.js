90059a53c27ca8cf1b0a3bd06ebb2726f51ac18d
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-04-21 12:19:13 -0700

    updated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -62,7 +62,7 @@ class Header extends Component {
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
             <h3>
-              I'm a {city} based <span>{occupation}</span>. {description}.
+              I'm a {city} based <span>{occupation}</span>. {description}
             </h3>
             <hr />
             <ul className="social">{networks}</ul>
